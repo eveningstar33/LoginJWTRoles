572ffc43077e1086b4b4199b0edf0d774edcc70c
Dragos Sarpe <contact@usdgadget.com>
2019-01-17 14:18:51 +0200

    Initial import of Spring Boot app

---
+++ src/main/java/com/dgs/springbootjwtauth/SpringbootJwtauthApplication.java
@@ -0,0 +1,14 @@
+package com.dgs.springbootjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtauthApplication.class, args);
+	}
+
+}
+
